1b523fd1d90f5bfcabe3f8400977b1ad16ad3715
wshwsh12 <793703860@qq.com>
2020-04-10 01:07:24 +0800

    fix

--- dbms/src/Flash/Coprocessor/InterpreterDAG.h
+++ dbms/src/Flash/Coprocessor/InterpreterDAG.h
@@ -113,7 +113,7 @@ private:
     Int32 handle_col_id = -1;
     std::vector<const tipb::Expr *> conditions;
 
-    std::vector<UInt64> join_key[100];
+    std::vector<String> join_key[100];
     std::shared_ptr<BloomFilter> bf[100];
 
     Poco::Logger * log;
