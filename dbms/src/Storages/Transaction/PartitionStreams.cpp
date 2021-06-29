fb98daff682d0eb45829a59b4ece1c927ee04eea
lidezhu <lidezhu@pingcap.com>
2021-06-29 19:30:21 +0800

    refine log

--- dbms/src/Storages/Transaction/PartitionStreams.cpp
+++ dbms/src/Storages/Transaction/PartitionStreams.cpp
@@ -110,7 +110,7 @@ static void writeRegionDataToStorage(
             }
 
             GET_METRIC(metrics, tiflash_raft_write_data_to_storage_duration_seconds, type_decode).Observe(region_decode_cost / 1000.0);
-            LOG_DEBUG(log, FUNCTION_NAME << ": region decode cost " << region_decode_cost << " milliseconds, decode rows " << block.rows() << "raw data size " << data_list_read.size());
+            LOG_DEBUG(log, FUNCTION_NAME << ": region decode cost " << region_decode_cost << " milliseconds, decode rows " << block.rows() << "raw data size " << data_list_read.size() << " column size " << storage->getTableInfo().columns.size());
             for (const auto & [pk, write_type, commit_ts, value_ptr] : data_list_read)
             {
                 std::ignore = value_ptr;
