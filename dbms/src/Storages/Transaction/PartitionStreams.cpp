c3114d5024bd50d3af76f657d51a49d9b754b25f
lidezhu <lidezhu@pingcap.com>
2021-07-01 18:16:37 +0800

    set high thread priority when decode

--- dbms/src/Storages/Transaction/PartitionStreams.cpp
+++ dbms/src/Storages/Transaction/PartitionStreams.cpp
@@ -17,6 +17,11 @@
 #include <Storages/Transaction/Utils.h>
 #include <common/logger_useful.h>
 
+#if defined(OS_LINUX)
+#   include <sys/time.h>
+#   include <sys/resource.h>
+#endif
+
 namespace DB
 {
 namespace FailPoints
@@ -41,6 +46,10 @@ static void writeRegionDataToStorage(
     auto metrics = context.getTiFlashMetrics();
     TableID table_id = region->getMappedTableID();
     UInt64 region_decode_cost = -1, write_part_cost = -1;
+#if defined(OS_LINUX)
+    if (0 != setpriority(PRIO_PROCESS, thread_id, -20))
+        throwFromErrno("Cannot 'setpriority'", ErrorCodes::CANNOT_SET_THREAD_PRIORITY);
+#endif
 
     /// Declare lambda of atomic read then write to call multiple times.
     auto atomicReadWrite = [&](bool force_decode) {
@@ -174,6 +183,10 @@ static void writeRegionDataToStorage(
             throw Exception("Write region " + std::to_string(region->id()) + " to table " + std::to_string(table_id) + " failed",
                 ErrorCodes::LOGICAL_ERROR);
     }
+#if defined(OS_LINUX)
+    if (0 != setpriority(PRIO_PROCESS, thread_id, 0))
+        throwFromErrno("Cannot 'setpriority'", ErrorCodes::CANNOT_SET_THREAD_PRIORITY);
+#endif
 }
 
 std::variant<RegionDataReadInfoList, RegionException::RegionReadStatus, LockInfoPtr> resolveLocksAndReadRegionData(
