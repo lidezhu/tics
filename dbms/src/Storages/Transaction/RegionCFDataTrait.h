16b5477f715ca258465755a1bd769350a76e9354
lidezhu <lidezhu@pingcap.com>
2022-05-04 11:43:08 +0800

    avoid decode lock cf

--- dbms/src/Storages/Transaction/RegionCFDataTrait.h
+++ dbms/src/Storages/Transaction/RegionCFDataTrait.h
@@ -94,8 +94,10 @@ struct RegionLockCFDataTrait
     {
         auto key = std::make_shared<const TiKVKey>(std::move(key_));
         auto value = std::make_shared<const TiKVValue>(std::move(value_));
+        // return {{key, std::string_view(key->data(), key->dataSize())},
+        //     Value{key, value, std::make_shared<const DecodedLockCFValue>(key, value)}};
         return {{key, std::string_view(key->data(), key->dataSize())},
-            Value{key, value, std::make_shared<const DecodedLockCFValue>(key, value)}};
+             Value{key, value, nullptr}};
     }
 };
 
