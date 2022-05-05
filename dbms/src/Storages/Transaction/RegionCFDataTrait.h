1addbddfcd864886254af9e4168a7326fffbf6d1
lidezhu <lidezhu@pingcap.com>
2022-05-05 21:04:19 +0800

    add parallel-hashmap

--- dbms/src/Storages/Transaction/RegionCFDataTrait.h
+++ dbms/src/Storages/Transaction/RegionCFDataTrait.h
@@ -18,6 +18,8 @@
 
 #include <map>
 
+#include "parallel_hashmap/phmap.h"
+
 namespace DB
 {
 
@@ -88,16 +90,14 @@ struct RegionLockCFDataTrait
     };
     using DecodedLockCFValue = RecordKVFormat::DecodedLockCFValue;
     using Value = std::tuple<std::shared_ptr<const TiKVKey>, std::shared_ptr<const TiKVValue>, std::shared_ptr<const DecodedLockCFValue>>;
-    using Map = std::unordered_map<Key, Value, Key::Hash>;
+    using Map = phmap::flat_hash_map<Key, Value, Key::Hash>;
 
     static Map::value_type genKVPair(TiKVKey && key_, TiKVValue && value_)
     {
         auto key = std::make_shared<const TiKVKey>(std::move(key_));
         auto value = std::make_shared<const TiKVValue>(std::move(value_));
-        // return {{key, std::string_view(key->data(), key->dataSize())},
-        //     Value{key, value, std::make_shared<const DecodedLockCFValue>(key, value)}};
         return {{key, std::string_view(key->data(), key->dataSize())},
-             Value{key, value, nullptr}};
+            Value{key, value, std::make_shared<const DecodedLockCFValue>(key, value)}};
     }
 };
 
