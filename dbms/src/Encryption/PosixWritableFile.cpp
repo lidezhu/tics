b0938634c27675c78050782ff310744f16011b83
lidezhu <lidezhu@pingcap.com>
2023-06-02 10:40:08 +0800

    try use fdatasync

--- dbms/src/Encryption/PosixWritableFile.cpp
+++ dbms/src/Encryption/PosixWritableFile.cpp
@@ -138,7 +138,7 @@ void PosixWritableFile::doOpenFile(bool truncate_when_exists_, int flags, mode_t
 int PosixWritableFile::fsync()
 {
     ProfileEvents::increment(ProfileEvents::FileFSync);
-    return ::fsync(fd);
+    return ::fdatasync(fd);
 }
 
 int PosixWritableFile::ftruncate(off_t length)
